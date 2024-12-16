a183381b37d2ae983849c89884974e94f24cf42f
Rohini0518 <rohinipolina0518@gmail.com>
2024-12-16 21:59:53 +0530

    Modified few code lines in youtubevideo file

--- src/VMgamifiedTailwind/Components/YouTubeVideo.jsx
+++ src/VMgamifiedTailwind/Components/YouTubeVideo.jsx
@@ -1,54 +1,58 @@
-import { useRef } from 'react';
-import YouTube from 'react-youtube';
+import { useRef } from "react";
+import YouTube from "react-youtube";
 
-export default function YouTubeVideo(){
+export default function YouTubeVideo() {
+  const playerRef = useRef(null);
 
-    const playerRef= useRef(null);
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+  const onReady = (event) => {
+    playerRef.current = event.target;
+  };
 
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-          autoplay: 0,
-        },
-      };
-      const onReady = (event) => {
-        playerRef.current=event.target;
-      };
+  const handlePlay = () => {
+    console.log(playerRef.current);
+    playerRef.current.playVideo();
+  };
+  const handlePause = () => {
+    console.log(playerRef.current);
+    playerRef.current.pauseVideo();
+  };
 
-      const handlePlay=()=>{
-        console.log(playerRef.current);
-        playerRef.current.playVideo();
-      }
-      const handlePause=()=>{
-        console.log(playerRef.current);
-        playerRef.current.pauseVideo();
-      }
+  const handleReset = () => {
+    playerRef.current.seekTo(0);
+    playerRef.current.pauseVideo();
+  };
 
-      const handleReset=()=>{
-        playerRef.current.seekTo(0)
-        playerRef.current.pauseVideo();
-      }
-    
-      const handleRepeat=()=>{
-        playerRef.current.seekTo(0)
-        playerRef.current.playVideo()
-      }
+  const handleRepeat = () => {
+    playerRef.current.seekTo(0);
+    playerRef.current.playVideo();
+  };
 
-    return(
-        <div>
-   <YouTube videoId="A5QEcmWLDVM" opts={opts} onReady={onReady} />
-   <button onClick={handlePlay} className="m-4 text-red-700 font-bold  py-2 px-4 border border-blue-500 rounded hover:bg-red-500 hover:text-yellow-300">
-  Play
-</button>    <button onClick={handlePause} className="m-4 text-red-700 font-bold  py-2 px-4 border border-blue-500 rounded hover:bg-red-500 hover:text-yellow-300">
-  Pause
-</button>   <button onClick={handleReset}  className="m-4 text-red-700 font-bold  py-2 px-4 border border-blue-500 rounded hover:bg-red-500 hover:text-yellow-300">
-  Reset
-</button>   <button onClick={handleRepeat} className="m-4 text-red-700 font-bold  py-2 px-4 border border-blue-500 rounded hover:bg-red-500 hover:text-yellow-300">
-  Repeat
-</button>  
+  const regularButtons = [
+    { label: "Play", onClick: handlePlay },
+    { label: "Pause", onClick: handlePause },
+    { label: "Reset", onClick: handleReset },
+    { label: "Repeat", onClick: handleRepeat },
+  ];
 
-
-        </div>
-    )
+  return (
+    <div>
+      <YouTube videoId="A5QEcmWLDVM" opts={opts} onReady={onReady} />
+      {buttons.map((button, index) => (
+      <button
+        key={index}
+        onClick={button.onClick}
+        className="m-4 text-red-700 font-bold py-2 px-4 border border-blue-500 rounded hover:bg-red-500 hover:text-yellow-300"
+      >
+        {button.label}
+      </button>
+    ))}
+    </div>
+  );
 }
